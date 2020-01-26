88b7c8b14d467a06626e49c798ab6894b5cd76a7
Jeremy Decker <jdecker71@outlook.com>
2020-01-26 15:06:47 -0500

    ui fixes css

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -19,6 +19,11 @@ export class SideNavComponent implements OnInit {
     private mediaMatcher: MediaQueryList = matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT}px`);
     private iphoneMediaMatcher: MediaQueryList = matchMedia(`(min-device-width : 375px) 
     and (max-device-width : 667px)`);
+    private iphone8MediaMatcher: MediaQueryList = matchMedia(`(min-device-width : 375px) 
+    and (max-device-width : 667px)`);
+    private iphoneXMediaMatcher: MediaQueryList = matchMedia(`and (device-width : 375px) 
+    and (device-height : 812px) 
+    and (-webkit-device-pixel-ratio : 3)`);
         
 
     constructor(private zone: NgZone, private breakpointObserver: BreakpointObserver, private data: DataService, private router: Router) {
@@ -27,7 +32,27 @@ export class SideNavComponent implements OnInit {
 
         this.iphoneMediaMatcher.addListener(mql => 
             zone.run(() => this.iphoneMediaMatcher = matchMedia(`(min-device-width : 375px) 
-            and (max-device-width : 667px)`)));
+            and (max-device-width : 667px)
+            and (-webkit-device-pixel-ratio : 2)`)));
+
+            this.iphone8MediaMatcher.addListener(mql => 
+                zone.run(() => this.iphoneMediaMatcher = matchMedia(`(min-device-width : 414px) 
+                and (max-device-width : 736px)
+                and (-webkit-device-pixel-ratio : 3)`)));
+
+                this.iphone8MediaMatcher.addListener(mql => 
+                    zone.run(() => this.iphoneMediaMatcher = matchMedia(`(min-device-width : 375px) 
+                    and (max-device-width : 812px)
+                    and (-webkit-device-pixel-ratio : 3)`)));
+
+            breakpointObserver.observe([
+                Breakpoints.HandsetLandscape,
+                Breakpoints.HandsetPortrait
+              ]).subscribe(result => {
+                if (result.matches) {
+                 this.isScreenSmall() == true;
+                }
+              });
     }
 
     ngOnInit() {
@@ -46,7 +71,7 @@ export class SideNavComponent implements OnInit {
 
     isScreenSmall(): boolean {
         //console.log(this.mediaMatcher.matches);
-       if(this.iphoneMediaMatcher.matches == true || this.mediaMatcher.matches == true) {
+       if(this.iphoneMediaMatcher.matches == true || this.mediaMatcher.matches == true || this.iphone8MediaMatcher.matches == true || this.iphoneXMediaMatcher.matches == true) {
         this.navTrue = false;
         return true;
        }
